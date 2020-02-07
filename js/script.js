f14f2eeae20cd5e750aa97e9cf32ca88b8ec8268
hiddentotem <60638836+hiddentotem@users.noreply.github.com>
2020-02-07 10:01:32 +0700

    Update filter brand, search, dan order per item

--- js/script.js
+++ js/script.js
@@ -15,19 +15,14 @@ function tampilkanSemuaMenu() {
 // Jalankan fungsi untuk menampilkan semua data menu
 tampilkanSemuaMenu();
 
-// Tangkap menu nav-link ketika di klik
-$('.nav-link').on('click', function () {
-    // Ketika ada menu yang di klik, hapus semua class active
-    $('.nav-link').removeClass('active');
+// Tangkap brand nav-link ketika di klik
+$('.brand-item').on('click', function () {
 
-    // Kasih class active ke menu yang di klik
-    $(this).addClass('active');
-
-    // Tangkap isi dari menu yang diklik
+    // Tangkap isi dari brand yang diklik
     let kategori = $(this).html();
 
-    // Simpan isi menu yang di klik ke dalam h1
-    $('h1').html(kategori);
+    // Simpan isi brand yang di klik ke dalam h1
+    $('.brand-tag').html(kategori);
 
     // Jika kategori yang di klik adalah all meu
     if (kategori == 'All Brand') {
@@ -54,10 +49,43 @@ $('.nav-link').on('click', function () {
                 content += '<div class="col-md-4"><div class="card mb-3"><img src="' + data.foto + '" class="img-thumbnail"><div class="card-body"><h5 class="card-title">' + data.tipe + '</h5><h6 class="card-title">' + data.harga + ',-</h6><p class="card-text">Harga asli <strike class="text-danger">' + data.hargaAsli + ',-</strike></p><a href="#" class="btn btn-primary tombolDetail" data-tipe="' + data.tipe + '" data-toggle="modal" data-target="#exampleModal">Detail</a></div></div></div>';
             }
         });
+
         // lalu set isi dari daftarMenu mengikuti isi dari content
         $('#daftarMenu').html(content);
     });
 
+});
+
+// Ketika dia search
+$('#keyword').on("keyup", function () {
+    let keyword = $(this).val();
+
+    if (keyword === '') {
+        $('#daftarMenu').html('');
+
+        tampilkanSemuaMenu();
+
+        return;
+    }
+
+
+    $.getJSON('js/smartphone.json', function (hasil) {
+        let regex = new RegExp(keyword, "i");
+        let smartphone = hasil.smartphone;
+        let content = '';
+
+        $.each(smartphone, function (i, data) {
+
+            // Jika smartphone kategorinya == kategori yang di klik
+            if ((data.tipe.search(regex) != -1) || (data.harga.search(regex) != -1) || (data.ram.search(regex) != -1) || (data.batre.search(regex) != -1)) {
+                // maka contentnya akan berisi smartphone berdasarkan kategorinya aja
+                content += '<div class="col-md-4"><div class="card mb-3"><img src="' + data.foto + '" class="img-thumbnail"><div class="card-body"><h5 class="card-title">' + data.tipe + '</h5><h6 class="card-title">' + data.harga + ',-</h6><p class="card-text">Harga asli <strike class="text-danger">' + data.hargaAsli + ',-</strike></p><a href="#" class="btn btn-primary tombolDetail" data-tipe="' + data.tipe + '" data-toggle="modal" data-target="#exampleModal">Detail</a></div></div></div>';
+            }
+        });
+
+        // lalu set isi dari daftarMenu mengikuti isi dari content
+        $('#daftarMenu').html(content);
+    });
 });
 
 // Ketika detail di klik
@@ -69,6 +97,7 @@ $(document).on('click', '.tombolDetail', function () {
     $.getJSON('js/smartphone.json', function (hasil) {
         let smartphone = hasil.smartphone;
         let content = '';
+        let btnContent = '';
 
         $.each(smartphone, function (i, data) {
 
@@ -111,9 +140,13 @@ $(document).on('click', '.tombolDetail', function () {
                         </div>
                     </div>
                 </div>`;
+
+                btnContent += `<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button><a href="https://wa.me/6282385594718?text=Hi%20aq%20mo%20mesan%20` + data.tipe + `%20dong" target="_blank" class=" btn btn-primary">Mau ini</a>`;
             }
         });
+
         $('.modal-body').html(content);
+        $('.btncontent').html(btnContent);
     });
 });
 
